2f341627e88b66c86ff39098153a25223b731540
Alisson Lauffer <alissonvitortc@gmail.com>
2023-01-19 23:16:13 -0300

    run cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use reqwest::Client;
 use uuid::Uuid;
 
-
 fn format_rtt(rtt: f64) -> String {
     let rtt = rtt / 1000.0;
     format!("{:.1} ms", rtt)
@@ -14,8 +13,15 @@ async fn main() {
     let request1 = client.get("https://router.nextdns.io/?source=ping").send();
     let request2 = client.get("https://test-ipv6.nextdns.io/").send();
     let uuid = Uuid::new_v4().to_string();
-    let active_router = client.get(format!("https://{}.test.nextdns.io/", uuid)).send();
-    let active_server = active_router.await.unwrap().json::<serde_json::Value>().await.unwrap();
+    let active_router = client
+        .get(format!("https://{}.test.nextdns.io/", uuid))
+        .send();
+    let active_server = active_router
+        .await
+        .unwrap()
+        .json::<serde_json::Value>()
+        .await
+        .unwrap();
 
     let (response1, response2) = tokio::join!(request1, request2);
 
@@ -24,7 +30,13 @@ async fn main() {
 
     let network_supports_ipv6 = response2.text().await.unwrap() == "OK";
 
-    for server in response1.json::<serde_json::Value>().await.unwrap().as_array().unwrap() {
+    for server in response1
+        .json::<serde_json::Value>()
+        .await
+        .unwrap()
+        .as_array()
+        .unwrap()
+    {
         let server = server.as_object().unwrap();
         let server_name = server.get("server").unwrap().as_str().unwrap();
         let ipv4 = server.get("ipv4").unwrap().as_bool().unwrap();
@@ -37,7 +49,9 @@ async fn main() {
         };
 
         if ipv4 {
-            let request = client.get(format!("https://ipv4-{}.edge.nextdns.io/info", server_name)).send();
+            let request = client
+                .get(format!("https://ipv4-{}.edge.nextdns.io/info", server_name))
+                .send();
             let response = request.await.unwrap();
             let rjson = response.json::<serde_json::Value>().await.unwrap();
             let pop = rjson["pop"].as_str().unwrap();
@@ -46,7 +60,9 @@ async fn main() {
         }
 
         if ipv6 && network_supports_ipv6 {
-            let request = client.get(format!("https://ipv6-{}.edge.nextdns.io/info", server_name)).send();
+            let request = client
+                .get(format!("https://ipv6-{}.edge.nextdns.io/info", server_name))
+                .send();
             let response = request.await.unwrap();
             let rjson = response.json::<serde_json::Value>().await.unwrap();
             let pop = rjson["pop"].as_str().unwrap();
@@ -54,5 +70,4 @@ async fn main() {
             println!("{} {} (IPv6) {}", is_active, pop, format_rtt(rtt));
         }
     }
-        
 }
